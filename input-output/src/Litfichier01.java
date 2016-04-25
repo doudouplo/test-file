7b6541f53f8be20396ef1e9f19c50bdfb79b0de6
doudouplo <doudouplo@free.fr>
2016-04-25 17:38:03 +0200

    commit deb

---
+++ input-output/src/Litfichier01.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Litfichier01 {
+
+	public static void main(String[] args) {
+		
+		File fichier = new File("text.txt");
+//
+//		System.out.println(fichier.isFile());
+//		System.out.println(fichier.toString());
+//		System.out.println(fichier.list());
+		
+	//	FileReader 
+		
+	}
+
+}
