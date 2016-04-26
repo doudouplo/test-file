e4e9a2f889dcb3da2747f127064de934c28c4496
doudouplo <doudouplo@free.fr>
2016-04-26 10:37:44 +0200

    cc

--- input-output/src/CompteOctets.java
+++ input-output/src/CompteOctets.java
@@ -7,15 +7,18 @@ public class CompteOctets {
 	public static void main(String[] args) throws IOException {
 		InputStream in;
 		
-		if (args.length == 0 )
-			in = System.in;
-		else
-			in = new FileInputStream(args[0]);
+//		if (args.length == 0 )
+//			in = System.in;
+//		else
+//			in = new FileInputStream(args[0]);
+		
+		in = new FileInputStream("text.txt");
 		
 		int total = 0;
 		
 		while (in.read() != -1){
 			total++;
+			System.out.println((char)in.read());
 		}
 		System.out.println(total + "octest");
 	}
